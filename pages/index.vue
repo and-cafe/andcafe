8a01d5ad77d6aecb6ddc7c232bcaae4e55e0e6ff
and cafe <andcafe.kagoshima@gmail.com>
2020-07-02 19:22:33 +0900

    ドメイン取得までの間、Netlifyのドメインを使用する

--- pages/index.vue
+++ pages/index.vue
@@ -153,7 +153,7 @@ export default {
     return {
       site_name: site_name,
       site_kana: site_kana,
-      site_domain: 'example.com',
+      site_domain: 'vibrant-shannon-0d7887.netlify.app',
       instagram_id: 'and.018',
       isNotProduction: false,
       description: '鹿児島県出水市の'+site_name+' ('+site_kana+') です。',
