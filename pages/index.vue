565a9cdaeb65252494fe2274766460a8b42e203e
and cafe <andcafe.kagoshima@gmail.com>
2020-07-20 18:02:09 +0900

    地図を差し替えた

--- pages/index.vue
+++ pages/index.vue
@@ -120,7 +120,7 @@
 
           <div class="bg-gray-500 col-span-1 lg:col-span-2">
             <div class="relative h-0 overflow-hidden bg-gray-300 aspect-16x9">
-              <iframe class="absolute inset-0 w-full h-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3381.226528314074!2d130.3135155151641!3d32.06312128119349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x353fb60763984e1d%3A0xecbd7b4e5691e0d8!2z44CSODk5LTA0MDEg6bm_5YWQ5bO255yM5Ye65rC05biC6auY5bC-6YeO55S65aSn5LmF5L-d77yT77yX77yV77yW!5e0!3m2!1sja!2sjp!4v1595226867825!5m2!1sja!2sjp" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+              <iframe class="absolute inset-0 w-full h-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d845.3068167466354!2d130.31520178811914!3d32.06310129881883!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x353fb5166de689c1%3A0x55d62d9ee8726d69!2sand%20cafe!5e0!3m2!1sja!2sjp!4v1595234962384!5m2!1sja!2sjp" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
           </div>
         </div>
