9f1788f14e73954b70f902fa7daebef55e8ffd0e
and cafe <andcafe.kagoshima@gmail.com>
2020-08-03 21:41:17 +0900

    スマホ用メニュー文字サイズ調整コンポーネントを追加した(コミット忘れ)

---
+++ components/UpperText.vue
@@ -0,0 +1,16 @@
+<template>
+  <span>
+    {{ name.substring(0, 1) }}<span class="text-xs sm:text-lg md:text-2xl">{{ name.substring(1, name.length) }}</span>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  }
+}
+</script>
