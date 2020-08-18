5e5149ed289cc0a9f156b268c2cce2a2d7a9dfe6
and cafe <andcafe.kagoshima@gmail.com>
2020-08-18 17:34:19 +0900

    インフォメーションをコンポーネント化した

--- pages/index.vue
+++ pages/index.vue
@@ -19,55 +19,10 @@
     />
 
     <main class="relative mx-auto xl:max-w-screen-xl">
-      <section id="info" class="scrollFadeIn">
-        <div class="p-2 lg:p-8 grid grid-cols-2 sm:grid-cols-3">
-          <div class="hidden rounded-lg sm:col-span-1 sm:block">
-            <img data-src="/images/main01.jpg" class="rounded-lg lazyload">
-          </div>
-
-          <div class="px-2 col-span-2 lg:px-8">
-            <h2 class="text-3xl font-bold text-gray-900 md:text-5xl">INFORMATION</h2>
-            <div class="m-4">
-              <p>2020年7月26日に出水市へ移転オープンするand cafe (アンドカフェ)です。</p>
-              <p>(2020年6月末まで阿久根市にて営業しておりました)</p>
-
-              <p class="pt-4">タピオカドリンクやフラッペ・シェイクなどのドリンクスイーツを、テイクアウト販売しております。</p>
-              <p>その他にハンドメイドのドライフラワーやピアス・イアリングなど販売しております。</p>
-
-              <p class="pt-4">最新情報はInstagramでご覧いただけます。</p>
-            </div>
-
-            <dl class="flex flex-wrap mx-4 mt-4 scrollFadeIn" id="info_detail">
-              <dt class="w-1/3 mb-2 font-bold md:w-1/4">営業時間</dt>
-              <dd class="w-2/3 mb-2 md:w-3/4">
-                11:00-17:00
-              </dd>
-              <dt class="w-1/3 mb-2 font-bold md:w-1/4">店休日</dt>
-              <dd class="w-2/3 mb-2 md:w-3/4">
-                <div>第3日曜日 &amp; 不定休</div>
-
-                <div
-                  v-if="holiday"
-                  class="text-sm font-bold text-red-500"
-                >
-                  ※次回店休日は{{ holiday }}です。
-                </div>
-                <div
-                  v-else
-                  class="text-sm"
-                >
-                  (最新の店休日情報はInstagramをご確認ください)
-                </div>
-              </dd>
-              <dt class="w-1/3 mb-2 font-bold md:w-1/4">Instagram</dt>
-              <dd class="w-2/3 mb-2 md:w-3/4">
-                <div><a v-bind:href="'https://www.instagram.com/'+instagram_id" target="_blank" rel="noopener">@{{ instagram_id }}</a></div>
-                <a v-bind:href="'https://www.instagram.com/'+instagram_id" target="_blank" rel="noopener" class="button--grey">Instagramを開く</a>
-              </dd>
-            </dl>
-          </div>
-        </div>
-      </section>
+      <section-info
+        v-bind:instagram_id="instagram_id"
+        v-bind:holiday="holiday"
+      />
 
       <section id="access" class="scrollFadeIn">
         <!--
