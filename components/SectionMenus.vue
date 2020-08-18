fcd951f5e9c8cfdb12bf92368ca99e1446e6fafa
and cafe <andcafe.kagoshima@gmail.com>
2020-08-18 17:47:43 +0900

    商品メニューをコンポーネント化した

---
+++ components/SectionMenus.vue
@@ -0,0 +1,27 @@
+<template>
+  <section id="menus" class="p-2 lg:p-8">
+    <h2 class="text-3xl font-bold text-gray-900 md:text-5xl">MENU</h2>
+
+    <section>
+      <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
+        <menu-price title="SPECIAL" color="bg-yellow-500" v-bind:menuData="menus.special" />
+        <menu-price-item-list title="TAPIOCA" color="bg-yellow-500" v-bind:menuData="menus.tapioca" />
+        <menu-price-item-list title="FRAPPE" color="bg-yellow-500" v-bind:menuData="menus.frappe" />
+        <menu-price-item-list title="SHAKE" color="bg-yellow-500" v-bind:menuData="menus.shake" />
+        <menu-price-item-list title="TOPPING" color="bg-yellow-500" v-bind:menuData="menus.topping" />
+      </div>
+    </section>
+
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    menus: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -26,20 +26,9 @@
 
       <section-access />
 
-      <section id="menus" class="p-2 lg:p-8">
-        <h2 class="text-3xl font-bold text-gray-900 md:text-5xl">MENU</h2>
-
-        <section>
-          <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
-            <menu-price title="SPECIAL" color="bg-yellow-500" v-bind:menuData="menus.special" />
-            <menu-price-item-list title="TAPIOCA" color="bg-yellow-500" v-bind:menuData="menus.tapioca" />
-            <menu-price-item-list title="FRAPPE" color="bg-yellow-500" v-bind:menuData="menus.frappe" />
-            <menu-price-item-list title="SHAKE" color="bg-yellow-500" v-bind:menuData="menus.shake" />
-            <menu-price-item-list title="TOPPING" color="bg-yellow-500" v-bind:menuData="menus.topping" />
-          </div>
-        </section>
-
-      </section>
+      <section-menus
+        v-bind:menus="menus"
+      />
 
       <section id="coupons" class="min-h-screen p-2 lg:p-8" v-if="enableCoupon">
         <h2 class="text-3xl font-bold text-gray-900 md:text-5xl">COUPONS</h2>
