2813442b35916c1498d210138f59c9aedabb1c78
and cafe <andcafe.kagoshima@gmail.com>
2020-08-18 16:12:28 +0900

    ヒーローヘッダーをコンポーネント化した

---
+++ components/HeroHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="relative h-screen">
+    <div class="w-full h-screen bg-center bg-cover hero-bg-img">
+      <div class="absolute w-full h-screen bg-black bg-opacity-25"></div>
+      <h1 class="absolute w-full text-6xl font-bold text-center text-white hero-cover xl:text-7xl" id="site_name">
+        {{ site_name }}
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    site_name: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.hero-bg-img {
+  background-image: url('~assets/images/main.jpg');
+}
+
+.hero-cover {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+
+  /* Tailwind CSS Only */
+  max-width: initial;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -13,14 +13,10 @@
       />
     </header>
 
-    <section class="relative h-screen">
-      <div class="w-full h-screen bg-center bg-cover hero-bg-img" ref="heroElm">
-        <div class="absolute w-full h-screen bg-black bg-opacity-25"></div>
-        <h1 class="absolute w-full text-6xl font-bold text-center text-white hero-cover xl:text-7xl" id="site_name">
-          {{ site_name }}
-        </h1>
-      </div>
-    </section>
+    <hero-header
+      ref="heroElm"
+      v-bind:site_name="site_name"
+    />
 
     <main class="relative mx-auto xl:max-w-screen-xl">
       <section id="info" class="scrollFadeIn">
@@ -378,19 +374,6 @@ export default {
 </script>
 
 <style scoped>
-.hero-bg-img {
-  background-image: url('~assets/images/main.jpg');
-}
-
-.hero-cover {
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%,-50%);
-
-  /* Tailwind CSS Only */
-  max-width: initial;
-}
-
 .aspect-16x9 {
   padding-bottom: 56.25%;
 }
