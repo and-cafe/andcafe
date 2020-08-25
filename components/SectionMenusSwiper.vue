4561846e869aa00bc6a52253cf5d828c24e7f964
and cafe <andcafe.kagoshima@gmail.com>
2020-08-25 22:55:32 +0900

    imgタグのalt属性を追加して、スライダーの定義をJSONから読み出すようにした

--- components/SectionMenusSwiper.vue
+++ components/SectionMenusSwiper.vue
@@ -1,35 +1,23 @@
 <template>
   <swiper class="swiper" :options="swiperOption">
-    <swiper-slide><img src="/images/tapioca01.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/tapioca02.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/tapioca03.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/tapioca04.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/tapioca05.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/tapioca06.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/tapioca07.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/tapioca08.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/tapioca09.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/tapioca10.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/frappe01.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/frappe02.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/frappe03.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/frappe04.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/frappe05.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/frappe06.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/frappe07.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/frappe08.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/frappe09.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/shake01.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/shake02.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/shake03.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/special01.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
-    <swiper-slide><img src="/images/special02.jpg" class="rounded-none lg:rounded-lg"></swiper-slide>
+    <template v-for="(names, category) in menus">
+      <template v-for="(name, index) in names">
+        <swiper-slide>
+          <img
+            v-bind:src="getImagePath(category, index)"
+            v-bind:alt="name"
+          >
+        </swiper-slide>
+      </template>
+    </template>
   </swiper>
 </template>
 
 <script>
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
+
+import menusSwiperJSON from '~/assets/json/menus_swiper.json'
 
 export default {
   components: {
@@ -38,6 +26,7 @@ export default {
   },
   data() {
     return {
+      menus: menusSwiperJSON,
       swiperOption: {
         slidesPerView: 4,
         centeredSlides: true,
@@ -60,6 +49,11 @@ export default {
         },
       },
     }
+  },
+  methods: {
+    getImagePath(name, index) {
+      return '/images/' + name + (index+1).toString().padStart(2, '0') + '.jpg';
+    },
   },
 }
 </script>
