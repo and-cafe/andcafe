c78627be5b2e5adc9f55f21646520f395895b75a
and cafe <andcafe.kagoshima@gmail.com>
2020-12-07 13:36:33 +0900

    ホットメニューを追加した

--- components/MenuPrice.vue
+++ components/MenuPrice.vue
@@ -3,9 +3,19 @@
     <h3 class="inline-block px-6 py-2 mb-4 font-bold" v-bind:class="color">{{ title }}</h3>
 
     <template v-for="(item, index) in menuData">
-      <div class="flex m-1">
+      <div class="flex m-1" :key="index">
         <span class="text-xs font-bold text-red-500" v-if="item.new">NEW</span>
-        <span class="font-bold">{{ item.name }}</span>
+        <span class="font-bold">
+          {{ item.name }}
+          <template v-if="item.items">
+            <div class="mt-2 ml-4 text-sm">
+              下記のドリンクからお選びください。
+              <ul v-for="(itm, idx) in item.items" :key="idx" class="ml-4">
+                <li class="list-disc">{{ itm }}</li>
+              </ul>
+            </div>
+          </template>
+        </span>
         <span class="ml-auto font-bold">￥{{ item.price | addComma }}</span>
       </div>
     </template>
--- components/SectionMenus.vue
+++ components/SectionMenus.vue
@@ -6,6 +6,10 @@
 
     <section class="px-2 lg:px-8">
       <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
+        <menu-price title="HOT SANDWICH" color="bg-orange-500" v-bind:menuData="menus.hotsandwich" id="hotsandwich" />
+        <menu-price-item-list title="HOT TAPIOCA" color="bg-orange-500" v-bind:menuData="menus.hottapioca" id="hottapioca" />
+        <menu-price-item-list title="HOT DRINK" color="bg-orange-500" v-bind:menuData="menus.hotdrink" id="hotdrink" />
+
         <menu-price title="SPECIAL" color="bg-yellow-500" v-bind:menuData="menus.special" id="special" />
         <menu-price-item-list title="TAPIOCA" color="bg-yellow-500" v-bind:menuData="menus.tapioca" id="tapioca" />
         <menu-price-item-list title="FRAPPE" color="bg-yellow-500" v-bind:menuData="menus.frappe" id="frappe" />
